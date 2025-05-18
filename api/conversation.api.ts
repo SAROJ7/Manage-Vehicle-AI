ee9e870ea1d0ee2675a4195c5e93e31cab6dbb28
saroj7 <sarojsht377@gmail.com>
2025-05-19 01:33:49 +0545

    mod: Desktop Sidebar Signout button

--- api/conversation.api.ts
+++
@@ -1,9 +0,0 @@
-export const createMessage = async ({
-  conversationId,
-  data,
-}: {
-  conversationId: string;
-  data: { userId: string; message: { role: string; content: string } };
-}) => {
-  // Your implementation
-};
